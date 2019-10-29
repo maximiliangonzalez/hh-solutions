3789e0274e7c99db2e847b86d66ea09dc9d3e4b3
Maximilian Gonzalez <maximiliangonzalez@Maximilians-iMac.local>
2019-10-29 14:49:41 -0700

    working on solution for new intersections

--- hh wk4/match-words.js
+++ hh wk4/match-words.js
@@ -10,14 +10,35 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
+const matchWord1 = str => {
+    const words = str.match(/[A-Za-z]+/gi);
+    if (!words) {
+        return true;
+    }
+
+    const stack = [];
+    words.forEach(word => {
+        const reversedWord = word.split('').reverse().join('');
+        if (reversedWord.toLowerCase() === stack[stack.length - 1]) {
+            stack.pop();
+        } else {
+            stack.push(word.toLowerCase());
+        }
+    });
+    
+    return stack.length === 0;
+}
+
 const matchWord = str => {
     const stack = [];
     const letter = /[A-Za-z]/;
     let currentWord = '';
+
     for (let i = 0; i < str.length; i++) {
         if (letter.test(str[i])) {
             currentWord += str[i];
         } 
+        // for some reason, letter.test(undefined) returns true -___-
         if (currentWord !== '' && (!letter.test(str[i]) && letter.test(str[i - 1])) || (letter.test(str[i]) && i === str.length - 1)) {
             const reversedCurrentWord = currentWord.split('').reverse().join('').toLowerCase();
             if (stack[stack.length - 1] === reversedCurrentWord) {
@@ -28,7 +49,7 @@ const matchWord = str => {
             currentWord = '';
         }
     }
-    console.log(stack);
+    
     return stack.length === 0;
 }
 
